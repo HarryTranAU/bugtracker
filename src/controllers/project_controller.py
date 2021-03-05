425f207c287b54a8b55fd9ac2b03ad132fe979cf
Harry Tran <css012013@coderacademy.edu.au>
2021-03-06 00:30:24 +1100

    add project_index, project_single

--- src/controllers/project_controller.py
+++ src/controllers/project_controller.py
@@ -1,5 +1,5 @@
 from models.Project import Project
-from schemas.ProjectSchema import project_schema
+from schemas.ProjectSchema import project_schema, projects_schema
 from main import db
 from main import bcrypt
 from flask_jwt_extended import jwt_required, get_jwt_identity
@@ -9,7 +9,11 @@ project = Blueprint('project', __name__, url_prefix="/project")
 
 
 @project.route("/create", methods=["POST"])
+# @jwt_required()
 def project_create():
+    # current_user = get_jwt_identity()
+    # return jsonify(logged_in_as=current_user), 200
+
     project_fields = project_schema.load(request.json)
 
     new_project = Project()
@@ -22,9 +26,16 @@ def project_create():
     return jsonify(project_schema.dump(new_project))
 
 
-@project.route("/read", methods=["GET"])
-def project_get():
-    pass
+@project.route("/all", methods=["GET"])
+def project_index():
+    projects = Project.query.all()
+    return jsonify(projects_schema.dump(projects))
+
+
+@project.route("/<int:id>", methods=["GET"])
+def project_single(id):
+    project = Project.query.filter_by(id=id).first()
+    return jsonify(project_schema.dump(project))
 
 
 @project.route("/update", methods=["PUT", "PATCH"])
