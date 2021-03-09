2b0fd8163da4eaa6ab3d54f93e554ce2084092eb
Harry Tran <css012013@coderacademy.edu.au>
2021-03-09 21:52:35 +1100

    added error messages

--- src/controllers/user_controller.py
+++ src/controllers/user_controller.py
@@ -1,7 +1,7 @@
 from models.User import User
 from schemas.UserSchema import user_schema
 from main import db, bcrypt, login_manager
-from flask import Blueprint, abort, render_template, redirect, url_for
+from flask import Blueprint, abort, render_template, redirect, url_for, flash
 from forms import LoginForm, RegisterForm
 from flask_login import login_required, login_user, logout_user, current_user
 
@@ -19,16 +19,24 @@ def user_register():
     form = RegisterForm()
 
     if form.validate_on_submit():
-        user = User()
-        user.email = form.email.data
-        user.username = form.username.data
-        user.password = bcrypt.generate_password_hash(
-                            form.password.data).decode("utf-8")
+        dupeUser = User.query.filter_by(username=form.username.data).first()
+        dupeEmail = User.query.filter_by(username=form.email.data).first()
 
-        db.session.add(user)
-        db.session.commit()
+        if not dupeUser and not dupeEmail:
+            user = User()
+            user.email = form.email.data
+            user.username = form.username.data
+            user.password = bcrypt.generate_password_hash(
+                                form.password.data).decode("utf-8")
 
-        return redirect(url_for('user.user_login'))
+            db.session.add(user)
+            db.session.commit()
+
+            flash("User successfully registered")
+            return redirect(url_for('user.user_login'))
+
+        flash("Username/Email already exists")
+        return redirect(url_for('user.user_register'))
 
     return render_template("register.html", form=form)
 
@@ -41,7 +49,8 @@ def user_login():
         user = User.query.filter_by(username=form.username.data).first()
         if not user or not bcrypt.check_password_hash(
                        user.password, form.password.data):
-            return abort(401, description="Incorrect username or password")
+            flash("Incorrect Username or Password")
+            return redirect(url_for('user.user_login'))
         login_user(user)
         return redirect(url_for("user.dashboard"))
 
